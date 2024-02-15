888c077e018cdbca192d4dfc22c47aa261c9c345
Nicolas Schoonbroodt <Nicolas@mathworks.com>
2024-02-15 01:10:17 +0100

    Adding router and basic navigation menu

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,27 @@
 use leptos::*;
+use leptos_router::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     view! {
-        <main>
-            <div>
-                Hello, World!
+        <Router>
+            <div class="grid grid-cols-4">
+                <div class="flex justify-end col-span-1">
+                    <nav class="text-right">
+                        <ul>
+                            <li><A href="/" class="border-r-4 border-white">Home</A></li>
+                            <li><A href="/CTO" class="border-r-4 border-white">CTO</A></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col-span-3">
+                    <main class="col-span-3">
+                        <div>
+                            Hello, World!
+                        </div>
+                    </main>
+                </div>
             </div>
-        </main>
+        </Router>
     }
 }
